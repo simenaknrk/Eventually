49549fddb3d1a46ea645a6527f490e523580526b
Simen Aklestad <aklestad@gmail.com>
2023-01-31 10:57:18 +0100

    Refactored project to follow standard SPM structure. Refactored Package.swift and upped swift tools version.

--- Package.swift
+++ Package.swift
@@ -1,6 +1,13 @@
-// swift-tools-version:5.3
+// swift-tools-version: 5.7
 import PackageDescription
 
 let package = Package(
-    name: "Eventually"
+    name: "Eventually",
+    products: [
+        .library(name: "Eventually", targets: ["Eventually"])
+    ],dependencies: [],
+    targets: [
+        .target(name: "Eventually", dependencies: []),
+        .testTarget(name: "EventuallyTests", dependencies: [])
+    ]
 )
